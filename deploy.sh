5bbfa5b1cb72b99c0329945ff8686fcc311c5384
rodrigo <rodrigo@temporadafora.com>
2014-11-06 17:08:39 -0500

    deploy with hash

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,10 @@ echo $DEV_PRETTY_HASH
 
 sed -i -pie "s/\(deploy_version: \)\(\".*\"\)/\1\"$DEV_PRETTY_HASH\"/g" _config.yml
 jekyll build
+mv -f _config.yml-pie  _config.yml
 
-
-#git checkout master
-#cp -r _site/* . && rm -rf _site/ es/_posts pt_BR/_posts deploy.sh  _plugins/ .idea/
-#git add --all . && git commit -m "Regenerate files ( $DEVHASH )"
-#git checkout development && git clean -f -d
-#mkdir _site/ && jekyll build
+git checkout master
+cp -r _site/* . && rm -rf _site/ es/_posts pt_BR/_posts deploy.sh  _plugins/ .idea/
+git add --all . && git commit -m "Regenerate files ( $DEVHASH )"
+git checkout development && git clean -f -d
+mkdir _site/ && jekyll build
