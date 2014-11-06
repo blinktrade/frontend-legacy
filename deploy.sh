b7c1e4f1adf0fe12bf86a434ca1984cf0e01a47f
rodrigo <rodrigo@temporadafora.com>
2014-11-06 16:08:18 -0500

    including devhash into the commit message

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-jekyll build
-git checkout master
-cp -r _site/* . && rm -rf _site/ es/_posts pt_BR/_posts deploy.sh  _plugins/ .idea/
-git add --all . && git commit -m "Regenerate files (jekyll deployment)"
-git checkout development && git clean -f -d
-mkdir _site/ && jekyll build
+DEVHASH=`git log --pretty=format:'%h' -n 1`
+
+echo $DEVHASH
+
+#jekyll build
+#git checkout master
+#cp -r _site/* . && rm -rf _site/ es/_posts pt_BR/_posts deploy.sh  _plugins/ .idea/
+#git add --all . && git commit -m "Regenerate files ( $DEVHASH )"
+#git checkout development && git clean -f -d
+#mkdir _site/ && jekyll build
