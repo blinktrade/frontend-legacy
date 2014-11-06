b930f38a7afde1f8ec7fa3da9fa1724f5bec457c
rodrigo <rodrigo@temporadafora.com>
2014-11-06 17:03:14 -0500

    including git dev hash into the html

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,11 @@ DEV_PRETTY_HASH=`git log --pretty=format:'%h' -n 1`
 
 echo $DEV_HASH
 echo $DEV_PRETTY_HASH 
- 
-#jekyll build
+
+sed -i -pie "s/\(deploy_version: \)\(\".*\"\)/\1\"$DEV_PRETTY_HASH\"/g" _config.yml
+jekyll build
+
+
 #git checkout master
 #cp -r _site/* . && rm -rf _site/ es/_posts pt_BR/_posts deploy.sh  _plugins/ .idea/
 #git add --all . && git commit -m "Regenerate files ( $DEVHASH )"
