86db809c377566b56a22ab10337e5e955100c44d
rodrigo <rodrigo@blinktrade.com>
2014-11-08 16:38:57 -0500

    adjusting baseurl

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 SOURCE_BRANCH=`git rev-parse --abbrev-ref HEAD`
 DEV_HASH=`git log --format='%H' -n 1`
 DEV_PRETTY_HASH=`git log --pretty=format:'%h' -n 1`
+REMOTE_NAME=`git remote -v | head -n1 | awk '{print $2}' | sed -e 's,.*:\(.*/\)\?,,' -e 's/.*\/\(.*\)\.git/\1/'`
+
 
 if [ $# -eq 0 ]; then
   DEST_BRANCH=master
@@ -18,8 +20,18 @@ fi
 echo "Version: $DEV_HASH ( $DEV_PRETTY_HASH )"
 
 sed -i -pie "s/\(deploy_version: \)\(\".*\"\)/\1\"$DEV_PRETTY_HASH\"/g" _config.yml
+mv _config.yml-pie  _config.yml-pie-deploy  #just for debug purposes 
+if [[ $REMOTE_NAME =~ github.io$ ]]; then
+  sed -i -pie "s/\(baseurl: \)\(\".*\"\)/\1\"\"/g" _config.yml 
+else
+  sed -i -pie "s/\(baseurl: \)\(\".*\"\)/\1\"\/$REMOTE_NAME\"/g"  _config.yml
+fi
+mv _config.yml-pie  _config.yml-pie-baseurl # just for debug purposes 
+
 jekyll build
-mv -f _config.yml-pie  _config.yml
+rm -rf _config.yml-pie-deploy 
+rm -rf _config.yml-pie-baseurl
+git checkout _config.yml 
 
 git checkout $DEST_BRANCH
 cp -r _site/* . && rm -rf _site/ es/_posts pt_BR/_posts deploy.sh  _plugins/ .idea/
