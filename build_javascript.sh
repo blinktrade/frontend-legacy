483777c742e7c42509535ef43e376553a1186b87
rodrigo <rodrigo@blinktrade.com>
2014-12-04 19:57:56 -0500

    created whitedev theme

--- build_javascript.sh
+++ build_javascript.sh
@@ -8,6 +8,11 @@ sh build_release.sh en_US default
 sh build_release.sh es default
 sh build_release.sh pt_BR default
 
+echo "whitedev theme"
+sh build_release.sh en_US whitedev
+sh build_release.sh es whitedev
+sh build_release.sh pt_BR whitedev
+
 echo "ander theme"
 sh build_release.sh en_US ander
 sh build_release.sh es ander
