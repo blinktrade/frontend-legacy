96a7a919a9968c8ac33c7de68c84cabb7b20bbf4
Cesar Augusto D. Azevedo <cesardeazevedo@outlook.com>
2015-01-27 20:50:11 -0200

    released whitedev integration

--- build_javascript.sh
+++ build_javascript.sh
@@ -3,11 +3,11 @@
 echo "Building javascript"
 cd jsdev
 
-echo "default theme"
-sh build_release.sh en_US default
-sh build_release.sh es default
-sh build_release.sh pt_BR default
-sh build_release.sh ro default
+# echo "default theme"
+# sh build_release.sh en_US default
+# sh build_release.sh es default
+# sh build_release.sh pt_BR default
+# sh build_release.sh ro default
 
 echo "whitedev theme"
 sh build_release.sh en_US whitedev
@@ -15,11 +15,11 @@ sh build_release.sh es whitedev
 sh build_release.sh pt_BR whitedev
 sh build_release.sh ro whitedev
 
-echo "ander theme"
-sh build_release.sh en_US ander
-sh build_release.sh es ander
-sh build_release.sh pt_BR ander
-sh build_release.sh ro ander
+# echo "ander theme"
+# sh build_release.sh en_US ander
+# sh build_release.sh es ander
+# sh build_release.sh pt_BR ander
+# sh build_release.sh ro ander
 
 echo "done"
 cd ../
