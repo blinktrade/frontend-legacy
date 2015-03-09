50f42bc51ace34403fee9bed4a3040d2beb5359c
Rodrigo <rodrigo@blinktrade.com>
2015-03-09 04:47:34 -0700

    default theme

--- build_javascript.sh
+++ build_javascript.sh
@@ -8,6 +8,7 @@ sh build_release.sh en_US default
 sh build_release.sh es default
 sh build_release.sh pt_BR default
 sh build_release.sh ro default
+sh build_release.sh zn_CN default
 
 echo "whitedev theme"
 sh build_release.sh en_US whitedev
@@ -21,6 +22,7 @@ sh build_release.sh en_US ander
 sh build_release.sh es ander
 sh build_release.sh pt_BR ander
 sh build_release.sh ro ander
+sh build_release.sh zn_CN ander
 
 echo "done"
 cd ../
