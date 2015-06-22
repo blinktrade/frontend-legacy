e2ae031ff2d29c728812868065e6dc0f3c8ce634
Phil Trinh <ptrinh@me.com>
2015-06-23 02:58:39 +0800

    Added Vietnam provinces

--- jsdev/bitex/util/util.js
+++ jsdev/bitex/util/util.js
@@ -579,7 +579,7 @@ bitex.util.getCountries = function() {
     "VU": "Vanuatu",
     "VA": "Vatican City",
     "VE": "Venezuela",
-    "VN": "Vietnam",
+    "VN": ["Vietnam", "An Giang|Bà Rịa - Vũng Tàu|Bạc Liêu|Bắc Kạn|Bắc Giang|Bắc Ninh|Bến Tre|Bình Dương|Bình Định|Bình Phước|Bình Thuận|Cà Mau|Cao Bằng|Cần Thơ|Đà Nẵng|Đắk Lắk|Đắk Nông|Điện Biên|Đồng Nai|Đồng Tháp|Gia Lai|Hà Giang|Hà Nam|Hà Nội|Hà Tây|Hà Tĩnh|Hải Dương|Hải Phòng|Hòa Bình|Hồ Chí Minh|Hậu Giang|Hưng Yên|Khánh Hòa|Kiên Giang|Kon Tum|Lai Châu|Lào Cai|Lạng Sơn|Lâm Đồng|Long An|Nam Định|Nghệ An|Ninh Bình|Ninh Thuận|Phú Thọ|Phú Yên|Quảng Bình|Quảng Nam|Quảng Ngãi|Quảng Ninh|Quảng Trị|Sóc Trăng|Sơn La|Tây Ninh|Thái Bình|Thái Nguyên|Thanh Hóa|Thừa Thiên - Huế|Tiền Giang|Trà Vinh|Tuyên Quang|Vĩnh Long|Vĩnh Phúc|Yên Bái", "An Giang|Bà Rịa - Vũng Tàu|Bạc Liêu|Bắc Kạn|Bắc Giang|Bắc Ninh|Bến Tre|Bình Dương|Bình Định|Bình Phước|Bình Thuận|Cà Mau|Cao Bằng|Cần Thơ|Đà Nẵng|Đắk Lắk|Đắk Nông|Điện Biên|Đồng Nai|Đồng Tháp|Gia Lai|Hà Giang|Hà Nam|Hà Nội|Hà Tây|Hà Tĩnh|Hải Dương|Hải Phòng|Hòa Bình|Hồ Chí Minh|Hậu Giang|Hưng Yên|Khánh Hòa|Kiên Giang|Kon Tum|Lai Châu|Lào Cai|Lạng Sơn|Lâm Đồng|Long An|Nam Định|Nghệ An|Ninh Bình|Ninh Thuận|Phú Thọ|Phú Yên|Quảng Bình|Quảng Nam|Quảng Ngãi|Quảng Ninh|Quảng Trị|Sóc Trăng|Sơn La|Tây Ninh|Thái Bình|Thái Nguyên|Thanh Hóa|Thừa Thiên - Huế|Tiền Giang|Trà Vinh|Tuyên Quang|Vĩnh Long|Vĩnh Phúc|Yên Bái"],
     "WF": "Wallis and Futuna",
     "EH": "Western Sahara",
     "YE": "Yemen",
