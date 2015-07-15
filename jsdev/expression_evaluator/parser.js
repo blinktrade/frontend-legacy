9d23f760e2a5a64a37e4b2058eb22b2172735266
Rodrigo Souza <pinhopro@users.noreply.github.com>
2015-07-15 15:38:31 -0400

    Update parser.js

--- jsdev/expression_evaluator/parser.js
+++ jsdev/expression_evaluator/parser.js
@@ -1,5 +1,13 @@
-goog.provide('expression_evaluator.Parser');
+/*!
+ Based on ndef.parser, by Raphael Graf(r@undefined.ch)
+ http://www.undefined.ch/mparser/index.html
+ Ported to JavaScript and modified by Matthew Crumley (email@matthewcrumley.com, http://silentmatt.com/)
+ You are free to use and modify this code in anyway you find useful. Please leave this comment in the code
+ to acknowledge its original source. If you feel like it, I enjoy hearing about projects that use my code,
+ but don't feel like you have to let me know or ask permission.
+*/
 
+goog.provide('expression_evaluator.Parser');
 
 goog.require('expression_evaluator.util');
 goog.require('expression_evaluator.functions');
