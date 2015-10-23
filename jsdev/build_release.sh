b0a2750fa97793d18f9e24907b02b4bf3f37527a
Rodrigo <rodrigo@blinktrade.com>
2015-10-23 18:47:56 -0400

    fixed build_release

--- jsdev/build_release.sh
+++ jsdev/build_release.sh
@@ -82,9 +82,6 @@ python ./closure-library/closure/bin/build/closurebuilder.py  \
   --compiler_flags="--externs=./externs/socket.io.js" \
   --compiler_flags="--externs=./externs/w3c_rtc.js" \
   --compiler_flags="--externs=./externs/facebook_javascript_sdk.js" \
-  --compiler_flags="--debug=TRUE" \
-  --compiler_flags="--formatting=PRETTY_PRINT" \
-  --compiler_flags="--formatting=PRINT_INPUT_DELIMITER" \
   --compiler_flags="--translations_file=./translations/$LANG.xtb.xml" \
    > ../assets/js/bitex_app_blink_trade.compiled.$LANG.$THEME.js
 
