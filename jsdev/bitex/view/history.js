3fcee86055ad8f8f5ce07a52376486301b35b8cf
Cesar  Azevedo <cesardeazevedo@outlook.com>
2015-10-23 18:42:05 -0400

    Finished HistoryView

--- jsdev/bitex/view/history.js
+++ jsdev/bitex/view/history.js
@@ -15,6 +15,8 @@ goog.require('bitex.templates');
 bitex.view.HistoryView = function(app, opt_domHelper) {
   bitex.view.View.call(this, app, opt_domHelper);
 
+  this.order_id_ = null;
+  this.client_order_id = null;
   this.request_order_id_ = null;
 };
 goog.inherits(bitex.view.HistoryView, bitex.view.View);
@@ -29,17 +31,6 @@ bitex.view.HistoryView.prototype.order_manager_table_;
  */
 bitex.view.HistoryView.prototype.request_order_id_;
 
-/**
- * @type {number}
- */
-bitex.view.HistoryView.prototype.market_data_subscription_id_;
-
-/**
- * @type {string}
- */
-bitex.view.HistoryView.prototype.market_data_subscription_symbol_;
-
-
 bitex.view.HistoryView.prototype.enterView = function() {
   goog.base(this, 'enterView');
   this.recreateComponents_();
@@ -51,38 +42,13 @@ bitex.view.HistoryView.prototype.exitView = function() {
 };
 
 /**
- * @param {Element} element Element to decorate.
- * @protected
- */
-bitex.view.HistoryView.prototype.decorateInternal = function(element){
-  this.setElementInternal(element);
-};
-
-/**
  * @private
  */
 bitex.view.HistoryView.prototype.destroyComponents_ = function(){
+
   var handler = this.getHandler();
 
-  if (goog.isDefAndNotNull(this.market_data_subscription_id_)) {
-    var conn = this.getApplication().getBitexConnection() ;
-    handler.unlisten( conn, bitex.api.BitEx.EventType.ORDER_BOOK_CLEAR + '.' + this.market_data_subscription_id_, this.onOBClear_);
-    handler.unlisten( conn, bitex.api.BitEx.EventType.ORDER_BOOK_DELETE_ORDERS_THRU + '.' + this.market_data_subscription_id_, this.onOBDeleteOrdersThru_);
-    handler.unlisten( conn, bitex.api.BitEx.EventType.ORDER_BOOK_DELETE_ORDER + '.' + this.market_data_subscription_id_, this.onOBDeleteOrder_);
-    handler.unlisten( conn, bitex.api.BitEx.EventType.ORDER_BOOK_UPDATE_ORDER + '.' + this.market_data_subscription_id_, this.onOBUpdateOrder_);
-    handler.unlisten( conn, bitex.api.BitEx.EventType.ORDER_BOOK_NEW_ORDER + '.' + this.market_data_subscription_id_, this.onOBNewOrder_);
-
-
-    this.dispatchEvent(bitex.view.View.EventType.MARKET_DATA_UNSUBSCRIBE);
-    this.market_data_subscription_id_ = null;
-    this.market_data_subscription_symbol_ = null;
-  }
-
   if (goog.isDefAndNotNull(this.order_manager_table_)) {
-
-    handler.unlisten(this.getApplication().getBitexConnection(),
-                     bitex.api.BitEx.EventType.EXECUTION_REPORT,
-                     this.onExecutionReport_ );
 
     handler.unlisten(this.order_manager_table_,
                      bitex.ui.DataGrid.EventType.REQUEST_DATA,
@@ -93,7 +59,6 @@ bitex.view.HistoryView.prototype.destroyComponents_ = function(){
                      this.onOrderListResponse_);
 
   }
-  goog.dom.removeChildren( goog.dom.getElement('trading_order_entry_content'));
 
   this.removeChildren(true);
   this.order_manager_table_ = null;
@@ -111,11 +76,7 @@ bitex.view.HistoryView.prototype.recreateComponents_ = function(){
 
   this.request_order_id_ = parseInt( 1e7 * Math.random() , 10 );
 
-  this.order_manager_table_ =  new bitex.ui.OrderManager('simple');
-
-  handler.listen(this.getApplication().getBitexConnection(),
-                 bitex.api.BitEx.EventType.EXECUTION_REPORT,
-                 this.onExecutionReport_);
+  this.order_manager_table_ = new bitex.ui.OrderManager('simple');
 
   handler.listen(this.order_manager_table_,
                  bitex.ui.DataGrid.EventType.REQUEST_DATA,
@@ -127,22 +88,7 @@ bitex.view.HistoryView.prototype.recreateComponents_ = function(){
 
   this.addChild(this.order_manager_table_, true);
 
-  this.order_manager_table_.setColumnFormatter('Side', this.orderFormatter_, this);
-  // this.order_manager_table_.setColumnFormatter('OrdStatus', this.simpleStatusFormatter_, this);
-  this.order_manager_table_.setColumnFormatter('AvgPx', this.avgPriceFormatter_, this);
-  this.order_manager_table_.setColumnFormatter('Volume', this.priceFormatter_, this);
-  handler.listen(this.order_manager_table_.getElement(), goog.events.EventType.CLICK, this.onCancelOrder_ );
-
-  this.market_data_subscription_id_ = parseInt( 1e7 * Math.random() , 10 );
-  this.market_data_subscription_symbol_ = selected_symbol.symbol;
-
-  var conn = this.getApplication().getBitexConnection() ;
-  handler.listen(conn, bitex.api.BitEx.EventType.ORDER_BOOK_CLEAR + '.' + this.market_data_subscription_id_, this.onOBClear_);
-  handler.listen(conn, bitex.api.BitEx.EventType.ORDER_BOOK_DELETE_ORDERS_THRU + '.' + this.market_data_subscription_id_, this.onOBDeleteOrdersThru_);
-  handler.listen(conn, bitex.api.BitEx.EventType.ORDER_BOOK_DELETE_ORDER + '.' + this.market_data_subscription_id_, this.onOBDeleteOrder_);
-  handler.listen(conn, bitex.api.BitEx.EventType.ORDER_BOOK_UPDATE_ORDER + '.' + this.market_data_subscription_id_, this.onOBUpdateOrder_);
-  handler.listen(conn, bitex.api.BitEx.EventType.ORDER_BOOK_NEW_ORDER + '.' + this.market_data_subscription_id_, this.onOBNewOrder_);
-  this.dispatchEvent(bitex.view.View.EventType.MARKET_DATA_SUBSCRIBE);
+  handler.listen(this.order_manager_table_.getElement(), goog.events.EventType.CLICK, this.onCancelOrder_);
 };
 
 bitex.view.HistoryView.prototype.enterDocument = function() {
@@ -152,96 +98,7 @@ bitex.view.HistoryView.prototype.enterDocument = function() {
   var model = this.getApplication().getModel();
 
   this.recreateComponents_();
-
 }
-
-/**
- * @param {*} value
- * @param {Object} rowSet
- */
-bitex.view.TradingView.prototype.avgPriceFormatter_ = function(value, rowSet) {
-  var priceCurrency = this.getApplication().getPriceCurrencyFromSymbol(rowSet['Symbol']);
-  if (value != 0) {
-    return this.getApplication().formatCurrency(value/1e8, priceCurrency, true);
-  } else {
-    return this.getApplication().formatCurrency(rowSet['Price']/1e8, priceCurrency, true);
-  }
-};
-
-/**
- * @param {*} value
- * @param {Object} rowSet
- */
-bitex.view.TradingView.prototype.qtyFormatter_ = function(value, rowSet) {
-  var priceCurrency = this.getApplication().getQtyCurrencyFromSymbol(rowSet['Symbol']);
-  return this.getApplication().formatCurrency(value/1e8, priceCurrency, true);
-};
-
-/**
- * @param {*} value
- * @param {Object} rowSet
- */
-bitex.view.TradingView.prototype.priceFormatter_ = function(value, rowSet) {
-  var priceCurrency = this.getApplication().getPriceCurrencyFromSymbol(rowSet['Symbol']);
-  return this.getApplication().formatCurrency(value/1e8, priceCurrency, true);
-};
-
-/**
- * @param {*} value
- * @param {Object} rowSet
- */
-bitex.view.HistoryView.prototype.simpleStatusFormatter_ = function(value, rowSet) {
-  /** @desc Order Status message on Simple Order Manager */
-  // var MSG_ORDER_MANAGER_SIMPLE_STATUS_SENDING = goog.getMsg('Sending...');
-
-  /** @desc Order Status message on Simple Order Manager */
-  // var MSG_ORDER_MANAGER_SIMPLE_STATUS_WAITING_BUYERS = goog.getMsg('Waiting buyers');
-
-  /** @desc Order Status message on Simple Order Manager */
-  // var MSG_ORDER_MANAGER_SIMPLE_STATUS_WAITING_SELLERS = goog.getMsg('Waiting sellers');
-
-  /** @desc Order Status message on Simple Order Manager */
-  // var MSG_ORDER_MANAGER_SIMPLE_STATUS_PARTIAL_BOUGHT = goog.getMsg('Partially bought');
-
-  /** @desc Order Status message on Simple Order Manager */
-  // var MSG_ORDER_MANAGER_SIMPLE_STATUS_PARTIAL_SOLD = goog.getMsg('Partially sold');
-
-  /** @desc Order Status message on Simple Order Manager */
-  // var MSG_ORDER_MANAGER_SIMPLE_STATUS_COMPLETE = goog.getMsg('Done');
-
-  /** @desc Order Status message on Simple Order Manager */
-  // var MSG_ORDER_MANAGER_SIMPLE_STATUS_CANCELLED = goog.getMsg('Cancelled');
-
-  /** @desc Order Status message on Simple Order Manager */
-  // var MSG_ORDER_MANAGER_SIMPLE_STATUS_REJECTED = goog.getMsg('Rejected');
-
-  // switch (value) {
-    // case '-': // Pending ...
-      // return MSG_ORDER_MANAGER_SIMPLE_STATUS_SENDING;
-    // case '0': // New
-      // if (rowSet['Side'] == '1') { // buying
-        // return MSG_ORDER_MANAGER_SIMPLE_STATUS_WAITING_SELLERS;
-      // } else { // selling
-        // return MSG_ORDER_MANAGER_SIMPLE_STATUS_WAITING_BUYERS;
-      // }
-    // case '1': // Partial fill
-      // if (rowSet['Side'] == '1') { // buying
-        // return MSG_ORDER_MANAGER_SIMPLE_STATUS_PARTIAL_BOUGHT;
-      // } else { // selling
-        // return MSG_ORDER_MANAGER_SIMPLE_STATUS_PARTIAL_SOLD;
-      // }
-    // case '2': // filled
-      // return MSG_ORDER_MANAGER_SIMPLE_STATUS_COMPLETE;
-    // case '4': // Cancelled
-      // if (rowSet['CumQty'] == 0 ) {
-        // return MSG_ORDER_MANAGER_SIMPLE_STATUS_CANCELLED;
-      // } else {
-        // return MSG_ORDER_MANAGER_SIMPLE_STATUS_COMPLETE;
-      // }
-    // case '8': // Rejected 
-      // return MSG_ORDER_MANAGER_SIMPLE_STATUS_REJECTED;
-  // }
-};
 
 /**
  *
@@ -266,73 +123,18 @@ bitex.view.HistoryView.prototype.onOrderListResponse_ = function(e) {
   this.order_manager_table_.setResultSet(msg['OrdListGrp'], msg['Columns']);
 };
 
-bitex.view.HistoryView.prototype.onOBClear_ = function(e){
-  this.bids_ = [];
-  this.asks_ = [];
-  this.ask_order_entry_.setOrderDepth(this.bids_);
-  this.bid_order_entry_.setOrderDepth(this.asks_);
-};
+/**
+ * @param {bitex.api.BitExEvent} e
+ * @private
+ */
+bitex.view.HistoryView.prototype.onCancelOrder_ = function(e) {
+  if (e.target.getAttribute('data-action') == 'cancel') {
+    e.stopPropagation();
+    e.preventDefault();
 
+    this.order_id_        = e.target.getAttribute('data-order-id');
+    this.client_order_id_ = e.target.getAttribute('data-client-order-id');
 
-bitex.view.HistoryView.prototype.onOBDeleteOrdersThru_ = function(e){
-  var msg   = e.data;
-  var index = msg['MDEntryPositionNo'];
-  var side  = msg['MDEntryType'];
-
-  if (side == '0') {
-    this.bids_.splice(0,index);
-    this.ask_order_entry_.setOrderDepth(this.bids_);
-  } else if (side == '1') {
-    this.asks_.splice(0,index);
-    this.bid_order_entry_.setOrderDepth(this.asks_);
+    this.dispatchEvent(bitex.view.View.EventType.CANCEL_ORDER);
   }
-};
-
-bitex.view.HistoryView.prototype.onOBDeleteOrder_ = function(e){
-  var msg   = e.data;
-  var index = msg['MDEntryPositionNo'] - 1;
-  var side  = msg['MDEntryType'];
-
-  if (side == '0') {
-    this.bids_.splice(index,1);
-    this.ask_order_entry_.setOrderDepth(this.bids_);
-  } else if (side == '1') {
-    this.asks_.splice(index,1);
-    this.bid_order_entry_.setOrderDepth(this.asks_);
-  }
-};
-
-bitex.view.HistoryView.prototype.onOBUpdateOrder_ = function(e){
-  var msg   = e.data;
-  var index = msg['MDEntryPositionNo'] - 1;
-  var qty   = msg['MDEntrySize'];
-  var side  = msg['MDEntryType'];
-
-  if (side == '0') {
-    this.bids_[index] = [ this.bids_[index][0], qty, this.bids_[index][2] ];
-    this.ask_order_entry_.setOrderDepth(this.bids_);
-  } else if (side == '1') {
-    this.asks_[index] = [ this.asks_[index][0], qty, this.asks_[index][2] ];
-    this.bid_order_entry_.setOrderDepth(this.asks_);
-  }
-};
-
-bitex.view.HistoryView.prototype.onOBNewOrder_ = function(e){
-  var msg       = e.data;
-  var index     = msg['MDEntryPositionNo'] - 1;
-  var price     = msg['MDEntryPx'];
-  var qty       = msg['MDEntrySize'];
-  var username  = msg['Username'];
-  var broker    = msg['Broker'];
-  var orderId   = msg['OrderID'];
-  var side      = msg['MDEntryType'];
-
-  if (side == '0') {
-    goog.array.insertAt( this.bids_, [price, qty, username], index );
-    this.ask_order_entry_.setOrderDepth(this.bids_);
-  } else if (side == '1') {
-    goog.array.insertAt( this.asks_, [price, qty, username], index );
-    this.bid_order_entry_.setOrderDepth(this.asks_);
-  }
-
 };
